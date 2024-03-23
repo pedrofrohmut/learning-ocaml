7e18473cd40e5bd72b9f38bb667c57b70c44dbf0
Pedro Frohmut <pedrofrohmut@gmail.com>
2024-03-23 14:23:30 -0300

    Problem 48 solved. Problem 46 renamed so the names can be used on the Problem 48 solution. Problem 46 names indicate that they work only with 2 arguments and problem 48 solution works with variable number of arguments

--- ninety_nine/lib/logics.ml
+++ ninety_nine/lib/logics.ml
@@ -31,7 +31,7 @@ type bool_expr =
   | And of bool_expr * bool_expr
   | Or of bool_expr * bool_expr
 
-let eval_bool_expr (symbol1 : string) (value1: bool) (symbol2: string) (value2 : bool) (expr: bool_expr) : bool =
+let eval_bool_expr2 (symbol1 : string) (value1: bool) (symbol2: string) (value2 : bool) (expr: bool_expr) : bool =
   let rec aux s1 v1 s2 v2 expr =
     match expr with
     | Var s -> if String.equal s s1 then v1 else v2
@@ -41,34 +41,76 @@ let eval_bool_expr (symbol1 : string) (value1: bool) (symbol2: string) (value2 :
   in
   aux symbol1 value1 symbol2 value2 expr
 
-let truth_table (a : string) (b : string) (expr : bool_expr) : (bool * bool * bool) list =
-  let res1 = eval_bool_expr a true b true expr in
-  let res2 = eval_bool_expr a true b false expr in
-  let res3 = eval_bool_expr a false b true expr in
-  let res4 = eval_bool_expr a false b false expr in
+let truth_table2 (a : string) (b : string) (expr : bool_expr) : (bool * bool * bool) list =
+  let res1 = eval_bool_expr2 a true b true expr in
+  let res2 = eval_bool_expr2 a true b false expr in
+  let res3 = eval_bool_expr2 a false b true expr in
+  let res4 = eval_bool_expr2 a false b false expr in
   [(true, true, res1); (true, false, res2); (false, true, res3); (false, false, res4)]
 
 (*
-48. Truth tables for logical expressions. (medium)
+  48. Truth tables for logical expressions. (medium)
 
-Generalize the previous problem in such a way that the logical expression may contain any number of logical variables. Define table in a way that table variables expr returns the truth table for the expression expr, which contains the logical variables enumerated in variables.
+  Generalize the previous problem in such a way that the logical expression may
+  contain any number of logical variables. Define table in a way that table
+  variables expr returns the truth table for the expression expr, which contains
+  the logical variables enumerated in variables.
 
-# table ["a"; "b"] (And (Var "a", Or (Var "a", Var "b")));;
-- : ((string * bool) list * bool) list =
-[([("a", true); ("b", true)], true); ([("a", true); ("b", false)], true);
- ([("a", false); ("b", true)], false); ([("a", false); ("b", false)], false)]
-# let a = Var "a" and b = Var "b" and c = Var "c" in
-  table ["a"; "b"; "c"] (Or (And (a, Or (b,c)), Or (And (a,b), And (a,c))));;
-- : ((string * bool) list * bool) list =
-[([("a", true); ("b", true); ("c", true)], true);
- ([("a", true); ("b", true); ("c", false)], true);
- ([("a", true); ("b", false); ("c", true)], true);
- ([("a", true); ("b", false); ("c", false)], false);
- ([("a", false); ("b", true); ("c", true)], false);
- ([("a", false); ("b", true); ("c", false)], false);
- ([("a", false); ("b", false); ("c", true)], false);
- ([("a", false); ("b", false); ("c", false)], false)]
+  # table ["a"; "b"] (And (Var "a", Or (Var "a", Var "b")));;
+  - : ((string * bool) list * bool) list =
+  [([("a", true); ("b", true)], true); ([("a", true); ("b", false)], true);
+  ([("a", false); ("b", true)], false); ([("a", false); ("b", false)], false)]
 
+  # let a = Var "a" and b = Var "b" and c = Var "c" in
+    table ["a"; "b"; "c"] (Or (And (a, Or (b,c)), Or (And (a,b), And (a,c))));;
+  - : ((string * bool) list * bool) list =
+  [ ([("a", true); ("b", true); ("c", true)], true)
+  ; ([("a", true); ("b", true); ("c", false)], true)
+  ; ([("a", true); ("b", false); ("c", true)], true)
+  ; ([("a", true); ("b", false); ("c", false)], false)
+  ; ([("a", false); ("b", true); ("c", true)], false)
+  ; ([("a", false); ("b", true); ("c", false)], false)
+  ; ([("a", false); ("b", false); ("c", true)], false)
+  ; ([("a", false); ("b", false); ("c", false)], false)
+  ]
+*)
+
+type truth_table_sample = (string * bool) list
+
+let gen_table (symbols: string list) : truth_table_sample list =
+  let rec gen tmp list =
+    match list with
+    | [] -> [List.rev tmp]
+    | x :: xs -> gen ((x, true) :: tmp) xs @ gen ((x, false) :: tmp) xs
+  in
+  gen [] symbols
+
+let eval_bool_expr (sample: truth_table_sample) (expr : bool_expr) : bool =
+  let rec get_bool_for symbol sample =
+    match sample with
+    | [] -> failwith "Symbol not in the list"
+    | (key, value) :: _ when String.equal key symbol -> value
+    | _ :: xs -> get_bool_for symbol xs
+  in
+  let rec aux sample expr =
+    match expr with
+    | Var symbol -> get_bool_for symbol sample
+    | Not x -> not (aux sample x)
+    | And (x, y) -> (aux sample x) && (aux sample y)
+    | Or (x, y) -> (aux sample x) || (aux sample y)
+  in
+  aux sample expr
+
+let truth_table (symbols : string list) (expr : bool_expr) : (truth_table_sample * bool) list =
+  let table = gen_table symbols in
+  let rec aux acc table expr =
+    match table with
+    | [] -> List.rev acc
+    | x :: xs -> let acc' = (x, eval_bool_expr x expr) :: acc in aux acc' xs expr
+  in
+  aux [] table expr
+
+(*
 49. Gray code. (medium)
 
 An n-bit Gray code is a sequence of n-bit strings constructed according to certain rules. For example,
--- ninety_nine/test/test_logics.ml
+++ ninety_nine/test/test_logics.ml
@@ -6,21 +6,44 @@ let suite46 =
     [ "example test" >:: (fun _ ->
         assert_equal
           [(true, true, true); (true, false, true); (false, true, false); (false, false, false)]
-          (Logics.truth_table "a" "b" (And (Var "a", Or (Var "a", Var "b")))))
+          (Logics.truth_table2 "a" "b" (And (Var "a", Or (Var "a", Var "b")))))
+    ]
+
+let suite48 =
+  let open Logics in
+  "Logic: Problem 48" >:::
+    [ "example test 2 symbols" >:: (fun _ ->
+        assert_equal
+          [([("a", true); ("b", true)], true); ([("a", true); ("b", false)], true);
+           ([("a", false); ("b", true)], false); ([("a", false); ("b", false)], false)]
+          (Logics.truth_table ["a"; "b"] (And (Var "a", Or (Var "a", Var "b")))))
+    ; "example test 3 symbols" >:: (fun _ ->
+          assert_equal
+            [ ([("a", true); ("b", true); ("c", true)], true)
+            ; ([("a", true); ("b", true); ("c", false)], true)
+            ; ([("a", true); ("b", false); ("c", true)], true)
+            ; ([("a", true); ("b", false); ("c", false)], false)
+            ; ([("a", false); ("b", true); ("c", true)], false)
+            ; ([("a", false); ("b", true); ("c", false)], false)
+            ; ([("a", false); ("b", false); ("c", true)], false)
+            ; ([("a", false); ("b", false); ("c", false)], false)
+            ]
+            (let a = Var "a" and b = Var "b" and c = Var "c" in
+             Logics.truth_table ["a"; "b"; "c"] (Or (And (a, Or (b,c)), Or (And (a,b), And (a,c))))))
     ]
 
 let () =
   let test_all = false in (* Flag to test all or a single test easy to change *)
   if not test_all then
-    let single_suite = suite46 in
+    let single_suite = suite48 in
     run_test_tt_main single_suite
   else
     let rec main_run suites =
       match suites with
       | [] -> ()
       | x :: xs ->
-        run_test_tt_main x;
-        main_run xs
+         run_test_tt_main x;
+         main_run xs
     in
-    let suites = [suite46] in
+    let suites = [suite46; suite48] in
     main_run suites
