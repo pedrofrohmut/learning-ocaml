c4640fd7e0c7a679ead4df273b35902bc0c77f40
pedrofrohmut <pedrofrohmut@gmail.com>
2026-07-15 13:58:24 -0300

    guess the number text

---
+++ algorithms/guess-the-number-game.ml
@@ -0,0 +1,14 @@
+(*
+  guess-the-number-game.ml
+  Pedro Frohmut 2026 Copyrights
+
+  Guess the number game is a cli game where the user enters a number between 1
+  and 100 and tries to guess what is the random generated number in the least
+  amount of tries.
+
+  The game validates the input. If valid the game checks if the value is lower,
+  bigger or entered the correct answer.
+
+  The game counts the user tries and show a score at the end based on the number
+  of tries the user did.
+ *)
